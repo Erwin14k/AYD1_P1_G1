f2b21cbdabdeb54a81b36142d3091bd112573cab
Kevin <3664028620101@ingenieria.usac.edu.gt>
2023-06-30 18:39:46 -0600

    Fixing Rating View

--- frontend/src/pages/Orders.js
+++ frontend/src/pages/Orders.js
@@ -113,7 +113,7 @@ function Orders({ noUrl }) {
                </RadioGroup>
             </FormControl>
             <br /> <br />
-            <TableContainer component={Paper}>
+            <TableContainer component={Paper} style={{marginBottom:"2%"}}>
                <Table>
                   <TableHead>
                      <TableRow>
@@ -156,7 +156,7 @@ function Orders({ noUrl }) {
                                  Ver
                               </Button>
 
-                              {orden.order_status === "Esperando" && (
+                              {orden.order_status === "Entregado" && (
                                  <ModalBaseRating
                                     order_id={orden.order_id}
                                     getPedidos={getPedidos}
